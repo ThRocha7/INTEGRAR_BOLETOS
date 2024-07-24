69f898380ae52ce40f6c01c1f4ed9011a9d92abd
Thiago Rocha <silva.throcha@gmail.com>
2024-07-24 16:57:02 -0300

    .

--- index.py
+++ index.py
@@ -32,6 +32,56 @@ def click_xpath(element):
     except:
         driver.quit()
 
-sign_on = '//*[@id="ssoBtn"]'
-click_xpath(sign_on)
+def write_xpath(element, text):
+    try:
+        text = str(text)
+        xpath = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, element))
+        )
+        xpath.send_keys(text)
+    except:
+        driver.quit()
 
+sign_on_btn = '//*[@id="ssoBtn"]'
+click_xpath(sign_on_btn)
+
+resources.email = input('email: ')
+field_email = '//*[@id="i0116"]'
+write_xpath(field_email, resources.email)
+
+avancar_btn = '//*[@id="idSIButton9"]'
+click_xpath(avancar_btn)
+
+resources.password= input('senha: ')
+field_password = '//*[@id="passwordInput"]'
+write_xpath(field_password, resources.password)
+
+entrar_btn = '//*[@id="submitButton"]'
+click_xpath(entrar_btn)
+
+nao_btn = '//*[@id="idBtn_Back"]'
+click_xpath(nao_btn)
+
+contas_pagar_btn = '//*[@id="groupNode_payables"]'
+click_xpath(contas_pagar_btn)
+
+pagamentos_btn = '//*[@id="itemNode_payables_payables_payments"]'
+click_xpath(pagamentos_btn)
+
+folha_btn = '//*[@id="pt1:_FOr1:1:_FOSritemNode_payables_payables_payments:0:_FOTsdi__PaymentLanding_itemNode__FndTasksList::icon"]'
+click_xpath(folha_btn)
+
+gerenciar_link = '//*[@id="pt1:_FOr1:1:_FOSritemNode_payables_payables_payments:0:_FOTRaT:0:RAtl23"]'
+click_xpath(gerenciar_link)
+
+adicionar_btn = '//*[@id="pt1:_FOr1:0:_FOSritemNode_payables_payables_payments:0:MAnt2:1:pt1:panelGroupLayout2:AT1:_ATp:create::icon"]'
+click_xpath(adicionar_btn)
+
+field_bu = '//*[@id="pt1:_FOr1:1:_FOSritemNode_payables_payables_payments:0:MAnt2:2:pt1:AP1:fcslov1:sis1:is1::content"]'
+write_xpath(field_bu, '123')
+
+field_num_doc = '//*[@id="pt1:_FOr1:1:_FOSritemNode_payables_payables_payments:0:MAnt2:2:pt1:AP1:it18::content"]'
+write_xpath(field_num_doc, '123')
+
+field_emissao = '//*[@id="pt1:_FOr1:1:_FOSritemNode_payables_payables_payments:0:MAnt2:2:pt1:AP1:id13::content"]'
+write_xpath(field_emissao, '12/01/2024')
--- resources/dados.py
+++ resources/dados.py
@@ -1,3 +1,3 @@
 email = ''
-senha = ''
+password = ''
 url = 'https://fa-evvi-saasfaprod1.fa.ocs.oraclecloud.com/fscmUI/faces/FuseWelcome?_adf.ctrl-state=gknd33cp2_687&_afrLoop=11190830558773601&_afrWindowMode=0&_afrWindowId=null&_afrFS=16&_afrMT=screen&_afrMFW=1422&_afrMFH=612&_afrMFDW=1280&_afrMFDH=720&_afrMFC=8&_afrMFCI=0&_afrMFM=0&_afrMFR=129&_afrMFG=0&_afrMFS=0&_afrMFO=0'
